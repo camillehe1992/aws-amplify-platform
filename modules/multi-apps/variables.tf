ce64bf5fe0aaab74bc0493bf95ea86b03dfcee1d
camillehe1992 <camille.he@outlook.com>
2024-08-02 15:16:35 +0800

    fix: update environment_variables type

--- modules/multi-apps/variables.tf
+++ modules/multi-apps/variables.tf
@@ -57,7 +57,7 @@ variable "branch_config" {
     branch_name                 = string
     enable_auto_build           = string
     enable_pull_request_preview = string
-    environment_variables       = optional(map(string), {})
+    environment_variables       = optional(map(object), {})
     secrets                     = optional(list(string), [])
   }))
   description = "The branch config of current platform"
